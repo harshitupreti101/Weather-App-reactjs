6e3da73cb5e339a739d46711126969d1b5b2bac4
harshitupreti101@gmail.com <harshiturpeti101>
2025-07-14 09:49:52 +0530

    Initial deploy for Weather App

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Weather-App-reactjs/',
   plugins: [react()],
-  base: '/<your-repo-name>/',
 })
